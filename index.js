8cba3c4474121b3a8896d791899dc18be2a06709
Elise <elise041294@gmail.com>
2022-04-09 15:18:17 -0600

    commit tree 50a830ad9945c51b5fc813ae6396de4d5b7a6c24

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
 // code your solution here
+// function superbowlWin(record){
+//     let answer = record.find(function(game){
+//         return game.result === "W"
+//     });
+//     if (answer){
+//         return answer.year;
+//     } else
+//         return undefined
+// };
+
+// function superbowlWin (record){
+//     let answer = record.find(function(game){
+//         return game.result === "W"
+//     });
+//     if (answer) {
+//         return answer.year 
+//     }
+// }
+
+
+function superbowlWin(record){
+    let answer = record.find(game => game.result === "W")
+    if (answer) {
+     return answer.year 
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function loopAndFind(result, finder){
+//     for(let item of result){
+//       if(finder(item) === true){
+//         outcome = item
+//         break
+//       }
+//     }
+//     return outcome
+//   }
+//   function superbowlWin(win){
+//     if(win.result === 'W'){
+//     console.log(superbowlWin)
+//     return win.year
+//     }
+//   }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// console.log(returnFirstTwoDrivers(drivers)) 
+
+// console.log(returnLastTwoDrivers(drivers))
+
+// const createFareMultiplier = function (number){
+//     return function (value){
+//         return value * number}
+// };
+// const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = function (drivers, driversToReturn) {
+//     return driversToReturn(drivers);
+// };
+
+  // };
+//     const returnLastTwoDrivers = function (drivers){
+//         return drivers.slice(-2 )
+//     }
+//     const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// console.log(returnFirstTwoDrivers(drivers)) 
+
+// console.log(returnLastTwoDrivers(drivers))
+
+// const createFareMultiplier = function (number){
+//     return function (value){
+//         return value * number}
+// };
+// const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = function (drivers, driversToReturn) {
+//     return driversToReturn(drivers);
+// };
